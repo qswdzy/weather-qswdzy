a150c6c6d9c820cc405ce7a43dc891f493521b53
qswdzy <qswdzy@163.com>
2016-02-28 21:17:02 +0800

    初始化天气项目代码

---
+++ weather/weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weather
+//
+//  Created by qswdzy on 16/2/28.
+//  Copyright © 2016年 qswdzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weather/weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weather
+//
+//  Created by qswdzy on 16/2/28.
+//  Copyright © 2016年 qswdzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
